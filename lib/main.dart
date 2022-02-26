2469b095db780287ff1b7b49cafe350338dc6c22
Wachirawit Roongmanie <roongmanie_w@silpakorn.edu>
2022-02-26 21:45:13 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,177 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+
+import 'InstaCargal.dart';
 import 'package:flutter/material.dart';
-import 'package:button/pin.dart';
 
 void main() {
-  runApp(const MyApp());
+  const app = MyApp();
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // callback method
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login',
+      title: 'InstaCargal',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
       ),
-      home: LoginPage(),
+      home: Test(),
+    );
+  }
+}
+
+class Test extends StatelessWidget {
+  Test({Key? key}) : super(key: key);
+
+  final List<Insta> InstaList = [
+    Insta(imguser: 'assets/images/shelbygt500logo.png', img: 'assets/images/shelbygt500.png',
+        userpost: 'BM', comments: [Comments(user: 'P_Peter', comment: "beautiful car"),Comments(user: 'C', comment: 'The best American muscle car ever made')],
+        caption: "Fastest mustang Ever \n#The new GT500"),
+    Insta(imguser: 'assets/images/Animalcrossing.png', img: 'assets/images/corvetteC8.png',
+        userpost: 'TT',comments: [Comments(user: 'Lucky', comment: 'very beautiful car'),Comments(user: 'B_B', comment: 'WOW! This car is very beautiful')],
+        caption: 'รถสวยมากก  \n#My new car'),
+    Insta(imguser: 'assets/images/Doraemon.png', img: 'assets/images/Figaro.png',
+        userpost: 'C',comments: [Comments(user: 'P_Peter', comment: 'รถ retro ที่โครตเก๋า'),Comments(user: 'David', comment: 'I like this car very much')],
+        caption: 'คัลท์ คาร์ จากยุค 90\n'),
+  ];
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xF5F1DBC4),
+      appBar: AppBar(title: Text('InstaCargal',
+          style: TextStyle(fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            fontSize: 25.0,))),
+      body: ListView.builder(
+          itemCount: InstaList.length,
+          itemBuilder: (context, index) =>
+              MyCard(insta: InstaList[index])),
+
+    );
+  }
+}
+class MyCard extends StatefulWidget {
+  final Insta insta;
+
+  const MyCard({
+    Key? key,
+    required this.insta,
+  }) : super(key: key);
+
+  @override
+  _MyCardState createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+  final controller = TextEditingController();
+  var like = false;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5.0,
+      margin: EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //Text(covid.date),
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 23,
+                  backgroundImage: AssetImage('${widget.insta.imguser}'),
+                ),
+                SizedBox(width: 10.0),
+                Expanded(
+                    child: Text(
+                        '${widget.insta.userpost}'
+                    )
+                ),
+                IconButton(onPressed: (){}, icon: Icon(Icons.more_vert),)
+              ],
+            ),
+          ),
+          Image.asset('${widget.insta.img}',fit: BoxFit.cover,),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                /*SizedBox(width: 5.0),*/
+                Expanded(
+                    child: Text(
+                        '${widget.insta.caption}'))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0,left: 16.0,right: 16.0),
+            child: Column(
+              children: [
+                for(int i=0;i<widget.insta.comments.length;i++)
+                  Row(
+                    children: [
+                      SizedBox(height: 23.0,),
+                      Text(
+                        '${widget.insta.comments[i].user}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      SizedBox(width: 5.0,),
+                      Text(
+                        '${widget.insta.comments[i].comment}',
+                        style: TextStyle(
+
+                        ),
+                      )
+                    ],
+                  ),
+              ],
+            ),
+          ),
+
+
+          Row(
+            children: [
+              IconButton(onPressed: (){
+                setState(() {
+                  like = !like;
+                });
+              }, icon: like? Icon(Icons.favorite,color: Colors.black,): Icon(Icons.favorite_border),),
+              Expanded(
+                child: TextField(
+                  onSubmitted: (value){
+
+                    setState(() {
+                      widget.insta.comments.add(Comments(user: 'Flutter', comment: value));
+                      controller.clear();
+
+                    });
+                  },
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    hintText: 'Add a comment',
+                  ),
+                ),
+              ),
+            ],
+          )
+
+
+        ],
+      ),
     );
   }
 }
